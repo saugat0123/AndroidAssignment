bcedf17ba0e40286139bbb259eef97e8d2145116
“[Saugat <[saugatdahal9@gmail.com]>
2020-12-03 18:24:01 +0545

    Classwork-1

--- src/classwork_1/Solution-18.kt
+++ src/classwork_1/Solution-18.kt
@@ -1,2 +1,13 @@
 package classwork_1
 
+fun main(){
+    val dictionaryApp = mapOf<String,String>(
+            "dog" to "first animal",
+            "cat" to "second animal",
+            "horse" to "third animal"
+    )
+
+    println("Enter a word:")
+    val word = readLine()!!
+    println("meaning of $word is: ${dictionaryApp[word]}")
+}
